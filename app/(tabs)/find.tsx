ae6723e6e1711ef77588382d346493cfecf20ff3
JeetDSharma <jeetsharma2112@gmail.com>
2026-02-26 21:43:12 -0500

    updated to bun

---
+++ app/(tabs)/find.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const find = () => {
+  return (
+    <View>
+      <Text>find Me</Text>
+    </View>
+  );
+};
+
+export default find;
+
+const styles = StyleSheet.create({});
