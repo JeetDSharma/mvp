78dfa7bbd0715238dc26747ad0ee80041f22ac17
JeetDSharma <jeetsharma2112@gmail.com>
2026-02-28 10:57:31 -0500

    config fixes

--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -5,9 +5,8 @@ interface EditScreenInfoProps {
 }
 
 export const EditScreenInfo: React.FC<EditScreenInfoProps> = ({ path }) => {
-  const title = 'Open up the code for this screen:';
-  const description =
-    'Change any of the text, save the file, and your app will automatically update.';
+  const title = 'Open up the code for this:';
+  const description = 'Jeet, let us build something';
 
   return (
     <View>
@@ -25,6 +24,6 @@ export const EditScreenInfo: React.FC<EditScreenInfoProps> = ({ path }) => {
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
   getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center`,
+  getStartedText: `text-xl leading-6 text-center`,
   homeScreenFilename: `my-2`,
 };
